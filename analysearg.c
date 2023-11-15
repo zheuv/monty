3e77e2390ef04b526cfa831630912af95fa5b15e
mouhcine wildane <muhssenwildane@gmail.com>
2023-11-15 23:04:30 +0100

    test

--- analysearg.c
+++ analysearg.c
@@ -1,37 +1,32 @@
 #include "monty.h"
 
-
-
-
-
 char* analysearg(char *arg)
 {
-        char *argument = malloc(MAX_LINE_LENGTH);
-        char *first_instance;
-	char *final_instance;
-        
-	while (*arg == ' ')
-        {
-                arg++;
-        }
-	first_instance = arg;
+    char *argument = malloc(MAX_LINE_LENGTH);
+    char *first_instance = NULL;
+    char *final_instance = NULL;
 
-        while (*arg != '\0')
+    while (*arg == ' ')
+    {
+        arg++;
+    }
+    first_instance = arg;
+
+    while (*arg != '\0' && *arg != '\n')
+    {
+        if (*arg != ' ')
         {
-                if (*arg != ' ')
-                {
-                        final_instance = arg;
-                }
-                arg++;
+            final_instance = arg;
         }
-        while (first_instance != final_instance)
-        {
-                strncat(argument, first_instance, 1);
-                first_instance++;
-        }
-        strncat(argument, final_instance, 1);
-        argument[-1] = '\0';
-        return argument;
+        arg++;
+    }
+
+    while (first_instance <= final_instance)
+    {
+        strncat(argument, first_instance, 1);
+        first_instance++;
+    }
+
+    return argument;
 }
-
 
