6a232b5ce0b482de69518e9a4ee7a23e16673cb9
mouhcine wildane <muhssenwildane@gmail.com>
2023-11-18 21:12:35 +0100

    0

--- isargint.c
+++ isargint.c
@@ -1,21 +1,23 @@
 #include "monty.h"
+#include <stdbool.h>
 #include <ctype.h>
 
 bool isargint(char *argument)
 {
-	if (*argument == '-')
-		argument++;
+    if (*argument == '-')
+        argument++;
 
-	while (*argument != '\0')
-	{
-		if (isdigit(*argument))
-		{
-			argument++;
-		}
-		else 
-		{
-			return false;
-		}
-	}
-	return true;
+    while (*argument != '\0')
+    {
+        if (isdigit((unsigned char)*argument))
+        {
+            argument++;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    return true;
 }
+
