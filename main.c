ade0bfc5fdeb2b0268241a6a972d76c06d29c02e
mouhcine wildane <muhssenwildane@gmai.com>
2023-11-18 14:01:52 -0800

    0

--- main.c
+++ main.c
@@ -8,7 +8,7 @@ int main(int argc, char *argv[])
 
 	if (argc != 2)
 	{
-		fprintf(stderr, "USAGE: monty file");
+		fprintf(stderr, "USAGE: monty file\n");
 		exit(EXIT_FAILURE);
 	}
 	filepath = argv[1];
