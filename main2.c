e6fb52c59f352dfe18d82599de236a2400746b8c
mouhcine wildane <muhssenwildane@gmail.com>
2023-11-18 08:18:19 -0800

    read

--- main2.c
+++ main2.c
@@ -1,27 +1,48 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include "monty.h"
 
-int main(int argc, char *argv[]) {
-    FILE *file = fopen(argv[1], "r");
+int main(int argc, char* argv[]) {
+    FILE* file = fopen(argv[1], "r");
+    char* command = malloc(sizeof(char) * 5);
+    char* argument;
+    int num_line = 0;
+    int i = 0;
 
     if (file == NULL) {
         perror("Error opening file");
         return 1;
     }
 
-    char *line = NULL;
+    char* line = NULL;
     size_t len = 0;
     ssize_t read;
 
     while ((read = getline(&line, &len, file)) != -1) {
-        char *token = strtok(line, "\t\n"); // Tab and line break as delimiters
+        char* token = strtok(line, " \t\n");
+        memset(command, 0, sizeof(char) * 5);
+        argument = NULL;
 
-        while (token != NULL) {
-            printf("Token: %s\n", token);
-            token = strtok(NULL, "\t\n");
+        while  (token != NULL) {
+            if (strlen(command) == 0) {
+                strncpy(command, token, 4);
+            } else if (argument == NULL) {
+                argument = token;
+		token = strtok(NULL, " \t\n");
+		break;
+            }
+
+            token = strtok(NULL, " \t\n");
         }
+	num_line++;
+	if (token != NULL)
+	{
+		printf("invalid argument in line %d\n", num_line);
+	}
+	else 
+	{
+		printf("%s %s\n", command, argument == NULL ? "(null)" : argument);
+	}
     }
+
 
     free(line);
     fclose(file);
